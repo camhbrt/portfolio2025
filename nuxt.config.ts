22374c2ea73d82d07a61b5fc82048f500846314d
Camille Hebert <camille.hebert22@gmail.com>
2024-12-27 14:07:25 +0100

    feat: :lipstick: update theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", href: "/daisy-logo.svg", type: "image/svg" }],
+    },
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
-})
+  modules: ["@nuxt/ui"],
+});
