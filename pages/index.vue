89f90d31a8e6dad5100c642782a9f9a9bd5fef47
Camille Hebert <camille.hebert22@gmail.com>
2024-12-29 16:46:13 +0100

    feat: :sparkles: add empty pages

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <UContainer>
+    <UCard class="mt-10">Home page </UCard>
+  </UContainer>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <p>Projects page</p>
+</template>
