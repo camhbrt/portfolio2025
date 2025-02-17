c4c9e8714575ff3161363366499609347935d540
Camille Hebert <camille.hebert22@gmail.com>
2025-02-18 00:50:06 +0100

    chore: :speech_balloon: add new texts for about page

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,27 +1,6 @@
 export default defineI18nConfig(() => ({
   locale: "fr",
   messages: {
-    en: {
-      layout: {
-        lang: "EN",
-        langSelect: "Translate to french",
-        themeSelect: "Change color theme",
-        showMenu: "Show menu",
-        home: "Home",
-        about: "About",
-        projects: "Projects",
-        contact: "Contact",
-        footer: `Made with ❤️ by Camille Hébert in 2025.`,
-      },
-      home: {
-        name: "Hi, I'm Camille",
-        job: "Full-stack web developer",
-        description:
-          "I am Camille Hébert, a Full-stack Developer specializing in front-end development. I am dedicated to accessibility and ensuring that web applications meet both user needs and client expectations. With two years of experience, I thrive on creating inclusive and impactful digital solutions. Welcome to my portfolio!",
-        resume: "Check my resume",
-        seeMore: "Learn more about me",
-      },
-    },
     fr: {
       layout: {
         lang: "FR",
@@ -32,7 +11,7 @@ export default defineI18nConfig(() => ({
         about: "À propos",
         projects: "Projets",
         contact: "Contact",
-        footer: `Créé avec ❤️ par Camille Hébert en 2025.
+        footer: `Créé avec ❤️ par Camille Hébert - 2025
         `,
       },
       home: {
@@ -42,6 +21,199 @@ export default defineI18nConfig(() => ({
           "Je suis Camille Hébert, développeuse Full-stack spécialisée en développement front-end. L'accessibilité est au cœur de mon travail, et je veille à ce que les applications web répondent aux besoins des utilisateurs et aux attentes des clients. Forte de deux ans d'expérience, je m'épanouis dans la création de solutions numériques inclusives et percutantes. Bienvenue sur mon portfolio !",
         resume: "Voir mon CV",
         seeMore: "En savoir plus",
+      },
+      career: {
+        background: "Mon parcours",
+        careerChange:
+          "Après six années passées à enseigner la musique au collège, j'ai décidé en 2023 de retourner sur les bancs de l'école et troquer le clavier de mon piano pour celui d'un ordinateur.",
+        teachingExperience:
+          "Mon expérience d'enseignante m'a appris à allier empathie et rigueur pour comprendre les besoins de mes élèves et les aider à surmonter leurs difficultés. Aujourd'hui, cette capacité d'adaptation me permet de concevoir des applications fiables, accessibles et intuitives, en phase avec les attentes des utilisateurs.",
+        currentJob:
+          "Depuis octobre 2023, j'exerce comme développeuse web chez Netatmo. J'y contribue à la création, l'optimisation et la maintenance de leurs sites web, en recherchant des solutions qui répondent aux besoins des utilisateurs et aux objectifs de l'entreprise, tout en m'assurant de l'accessibilité numérique.",
+      },
+      resume: {
+        label: "CV",
+        work: {
+          label: "Expériences professionnelles",
+          netatmo: {
+            date: "depuis oct. 2023",
+            title: "Développeuse web - NETATMO",
+            description:
+              "Depuis octobre 2023, j'exerce en tant que développeuse web en sein de l'équipe marketing de Netatmo, l'entreprise française leader de la maison connectée. Je participe au développement et au maintien des sites vitrine et e-commerce de la marque.",
+          },
+          chantemerle: {
+            date: "2017 - 2023",
+            title: "Professeure d'éducation musicale et chant choral",
+            description:
+              "Fonctionnaire rattachée au ministère de l'Education Nationale, j'ai enseigné pendant 6 ans, dont 5 au collège le Chantemerle de Corbeil-Essonnes (91). En charge de 20 classes par an (approx. 500 élèves), j'ai développé des compétences en gestion de groupes, gestion de projet, communication",
+          },
+          mcdonalds: {
+            date: "2013 - 2016",
+            title: "Equipière polyvalente chargée de dépôt",
+            description:
+              "Durant ma licence, j'ai travaillé chez McDonald's à Paris et Vigneux-sur-Seine (91) en tant qu'équipière polyvalente, en charge de la préparation des commandes, du service des clients et de la gestion des caisses, des stocks et dépôts d'argent en collaboration avec le manager.",
+          },
+        },
+        education: {
+          label: "Education",
+          ada: {
+            date: "2024",
+            title: "TP concepteur développeur d'applications",
+            description: "Ada Tech School, Paris (équivalent Bac+3)",
+          },
+          master: {
+            date: "2018",
+            title: "Master MEEF (enseignement) spécialité musique",
+            description:
+              "Université Rennes 2 / Institut National Supérieur du Professorat et de l'Education de Rennes",
+          },
+          licence: {
+            date: "2016",
+            title: "Licence de Musique et Musicologie",
+            description: "Université d'Evry-Val-d'Essonne",
+          },
+        },
+      },
+      skills: {
+        discoverProjects: "Découvrir mon travail",
+        skills: "Compétences",
+        softSkills: "Qualités",
+        hardSkills: "Compétences techniques",
+        languages: "Langues",
+        creativity: "Créativité",
+        empathy: "Empathie",
+        aestheticSense: "Sens esthétique",
+        organization: "Organisation",
+        adaptability: "Adaptabilité",
+        collaboration: "Collaboration",
+        responsiveDesign: "Responsive Design",
+        frontendFrameworks: "Frameworks front-end (React, Vue.js, Angular)",
+        design: "UI/UX Design",
+        prototyping: "Prototypage (Figma, Adobe XD)",
+        webAnimations: "Animations web (CSS, GSAP)",
+        webAccessibility: "Accessibilité web (WCAG)",
+        multimedia: "Multimédia (audio/vidéo en ligne)",
+        projectManagement: "Gestion de projet (Trello, Jira, Asana)",
+        socialAwareness: "Conscience sociale",
+        inclusionDiversity: "Inclusion et diversité",
+        ecoResponsibility: "Éco-responsabilité numérique",
+        digitalEthics: "Éthique numérique",
+        inclusiveCommunication: "Communication inclusive",
+        workEthic: "Éthique de travail",
+        detailOrientation: "Soucis du détail",
+        french: "Français",
+        english: "Anglais",
+        japanese: "Japonais",
+        mothersTongue: "langue maternelle",
+        advanced: "avancé (C1)",
+        beginner: "débutant (depuis 2025)",
+      },
+    },
+    en: {
+      layout: {
+        lang: "EN",
+        langSelect: "Translate to french",
+        themeSelect: "Change color theme",
+        showMenu: "Show menu",
+        home: "Home",
+        about: "About",
+        projects: "Projects",
+        contact: "Contact",
+        footer: `Made with ❤️ by Camille Hébert - 2025`,
+      },
+      home: {
+        name: "Hi, I'm Camille",
+        job: "Full-stack web developer",
+        description:
+          "I am Camille Hébert, a Full-stack Developer specializing in front-end development. I am dedicated to accessibility and ensuring that web applications meet both user needs and client expectations. With two years of experience, I thrive on creating inclusive and impactful digital solutions. Welcome to my portfolio!",
+        resume: "Check my resume",
+        seeMore: "Learn more about me",
+      },
+      career: {
+        background: "Background",
+        careerChange:
+          "After six years of teaching music in middle school, I decided in 2023 to go back to school and swap my piano keyboard for a computer keyboard.",
+        teachingExperience:
+          "My teaching experience has taught me to combine empathy and rigor to understand my students' needs and help them overcome their difficulties. Today, this ability to adapt allows me to design reliable, accessible, and intuitive applications that align with user expectations.",
+        currentJob:
+          "Since October 2023, I have been working as a web developer at Netatmo. I contribute to the creation, optimization, and maintenance of their websites, seeking solutions that meet user needs and business objectives while ensuring digital accessibility.",
+      },
+      resume: {
+        label: "Resume",
+        work: {
+          label: "Work Experience",
+          netatmo: {
+            date: "since Oct. 2023",
+            title: "Web Developer - NETATMO",
+            description:
+              "Since October 2023, I have been working as a web developer within the marketing team at Netatmo, the French leader in smart home technology. I participate in the development and maintenance of the brand's showcase and e-commerce websites.",
+          },
+          chantemerle: {
+            date: "2017 - 2023",
+            title: "Music and Choir Teacher",
+            description:
+              "As a civil servant under the French Ministry of National Education, I taught for six years, including five at Collège le Chantemerle in Corbeil-Essonnes (91). Managing 20 classes per year (approx. 500 students), I developed skills in group management, project management, and communication.",
+          },
+          mcdonalds: {
+            date: "2013 - 2016",
+            title: "Assistant Manager",
+            description:
+              "During my undergraduate studies, I worked at McDonald's in Paris and Vigneux-sur-Seine (91), handling order preparation, customer service, and managing cash registers, stock, and cash deposits in collaboration with the manager.",
+          },
+        },
+        education: {
+          label: "Education",
+          ada: {
+            date: "2024",
+            title: "Bachelor's degree - Software Developer",
+            description: "Ada Tech School, Paris",
+          },
+          master: {
+            date: "2018",
+            title: "Master's Degree in Teaching (MEEF) - Music Specialization",
+            description:
+              "Université Rennes 2 / Institut National Supérieur du Professorat et de l'Éducation de Rennes",
+          },
+          licence: {
+            date: "2016",
+            title: "Bachelor's Degree in Music and Musicology",
+            description: "Université d'Évry-Val-d'Essonne",
+          },
+        },
+      },
+      skills: {
+        discoverProjects: "Discover My Work",
+        skills: "Skills",
+        softSkills: "Soft Skills",
+        hardSkills: "Technical Skills",
+        languages: "Languages",
+        creativity: "Creativity",
+        empathy: "Empathy",
+        aestheticSense: "Aesthetic Sense",
+        organization: "Organization",
+        adaptability: "Adaptability",
+        collaboration: "Collaboration",
+        responsiveDesign: "Responsive Design",
+        frontendFrameworks: "Front-end Frameworks (React, Vue.js, Angular)",
+        design: "UI/UX Design",
+        prototyping: "Prototyping (Figma, Adobe XD)",
+        webAnimations: "Web Animations (CSS, GSAP)",
+        webAccessibility: "Web Accessibility (WCAG)",
+        multimedia: "Multimedia (Online Audio/Video)",
+        projectManagement: "Project Management (Trello, Jira, Asana)",
+        socialAwareness: "Social Awareness",
+        inclusionDiversity: "Inclusion & Diversity",
+        ecoResponsibility: "Digital Eco-Responsibility",
+        digitalEthics: "Digital Ethics",
+        inclusiveCommunication: "Inclusive Communication",
+        workEthic: "Work Ethic",
+        detailOrientation: "Attention to Detail",
+        french: "French",
+        english: "English",
+        japanese: "Japanese",
+        mothersTongue: "native language",
+        advanced: "advanced (C1)",
+        beginner: "beginner (since 2025)",
       },
     },
   },
