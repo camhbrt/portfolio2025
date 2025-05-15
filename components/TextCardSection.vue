63ab837b52174b50ac530b5aad441dc670e0929c
Camille Hébert <camille.hebert22@gmail.com>
2025-05-15 22:51:07 +0200

    feat: :sparkles: add highlights section to home page

---
+++ components/TextCardSection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+  defineProps<{
+    title: string;
+    cards: {
+      header?: string;
+      main: string;
+      footer?: string;
+      icon?: string;
+    }[]; //array of 3
+  }>();
+</script>
+<template>
+  <section class="grid grid-cols-12 my-24">
+    <h2 class="row-start-1 col-span-full text-4xl font-bold my-12">{{ title }}</h2>
+    <ul
+      class="w-full col-start-1 -col-end-1 lg:col-start-2 lg:-col-end-2 grid xs:grid-cols-2 md:grid-cols-3 gap-12"
+    >
+      <UCard
+        v-for="card in cards"
+        :key="card.main"
+        as="li"
+        variant="subtle"
+        class="text-center w-full shadow-2xl shadow-neutral-200 dark:shadow-neutral-900 [&>div]:h-full [&>div]:flex [&>div]:flex-col [&>div]:justify-center"
+      >
+        <p class="text-lg font-semibold">{{ card.header }}</p>
+        <p class="text-4xl text-primary font-bold">{{ card.main }}</p>
+        <p class="text-lg font-semibold">{{ card.footer }}</p>
+      </UCard>
+    </ul>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,68 @@
 <script setup lang="ts">
   const { t } = useI18n();
+
+  const cardsSection = {
+    title: t("home.highlights"),
+    cards: [
+      {
+        main: t("home.twoYears"),
+        footer: t("home.experience"),
+      },
+      {
+        header: t("home.master"),
+        main: t("home.vue"),
+        footer: t("home.vueTools"),
+      },
+      {
+        main: t("home.a11y"),
+        footer: t("home.accessibility"),
+      },
+    ],
+  };
 </script>
 <template>
-  <section class="grid grid-cols-12">
-    <div class="col-start-1 -col-end-1 md:col-end-7 md:my-auto flex flex-col gap-y-4 mt-12">
-      <p class="text-2xl font-bold">
-        {{ t("home.name") }}
-      </p>
-      <h1 class="text-4xl xxs:text-5xl font-bold text-pretty text-primary">{{ t("home.job") }}</h1>
-      <p class="text-pretty">{{ t("home.description") }}</p>
-      <div class="flex flex-wrap gap-4">
+  <main>
+    <!--  -->
+    <section class="grid grid-cols-12 my-20">
+      <NuxtImg
+        src="/portrait.svg"
+        alt="Portrait de Camille Hébert"
+        class="col-start-1 -col-end-1 xs:col-start-3 xs:-col-end-3 md:row-start-1 md:col-start-1 md:col-end-6 my-4 md:my-auto"
+      />
+      <div
+        class="col-start-1 -col-end-1 md:-col-end-1 md:col-start-7 md:my-auto flex flex-col gap-y-10 mt-12"
+      >
+        <p class="text-2xl font-bold">
+          {{ t("home.name") }}
+        </p>
+        <h1 class="text-4xl xxs:text-5xl font-bold text-pretty text-primary">
+          {{ t("home.job") }}
+        </h1>
+        <p class="text-pretty text-lg/relaxed">{{ t("home.description") }}</p>
+
         <UButton
           :label="t('home.resume')"
           color="neutral"
           variant="outline"
+          size="xl"
           target="_blank"
           :to="'/camille_hebert.pdf'"
-          class="px-12"
-        />
-        <UButton
-          :to="$localePath('/about')"
-          :label="t('home.seeMore')"
-          icon="i-heroicons:arrow-right"
-          color="neutral"
-          variant="link"
-          trailing
-          class="ml-auto transition-all ease-in-out hover:group-last:animate-ping"
+          class="px-12 w-fit self-center"
         />
       </div>
-    </div>
-    <NuxtImg
-      src="/portrait.svg"
-      alt="Portrait de Camille Hébert"
-      class="col-start-1 -col-end-1 xs:col-start-3 xs:-col-end-3 md:row-start-1 md:-col-end-1 md:col-start-8 my-4 md:my-auto"
+    </section>
+
+    <!-- Highlights Section -->
+    <TextCardSection v-bind="cardsSection" />
+
+    <UButton
+      :to="$localePath('/about')"
+      :label="t('home.seeMore')"
+      icon="i-heroicons:arrow-right"
+      color="neutral"
+      variant="link"
+      trailing
+      class="flex justify-end"
     />
-  </section>
+  </main>
 </template>
